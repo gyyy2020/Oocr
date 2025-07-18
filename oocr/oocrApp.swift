9930803fb4e217392c392acde9dac5bfe3c7edf1
gyyy2020 <workyg@outlook.com>
2025-07-18 14:28:03 +0800

    project init

---
+++ oocr/oocrApp.swift
@@ -0,0 +1,17 @@
+//
+//  oocrApp.swift
+//  oocr
+//
+//  Created by Atlantic on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct oocrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ oocrTests/oocrTests.swift
@@ -0,0 +1,17 @@
+//
+//  oocrTests.swift
+//  oocrTests
+//
+//  Created by Atlantic on 7/18/25.
+//
+
+import Testing
+@testable import oocr
+
+struct oocrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
